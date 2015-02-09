hlandauf/namecore

Introduce CPureBlockHeader.
Split the block header part that is used by auxpow and the "real" block
header (that uses auxpow) to resolve the cyclic dependency between the
two.

src/auxpow.h
@@ -10,11 +10,12 @@
 #include "serialize.h"
 #include "uint256.h"
 
-#include "core/block.h"
+#include "core/pureheader.h"
 #include "core/transaction.h"
 
 #include <vector>
 
+class CBlock;
 class CBlockIndex;
 
 /** Header for merge-mining data in the coinbase.  */
@@ -102,7 +103,7 @@ public:
   int nChainIndex;
 
   /** Parent block header (on which the real PoW is done).  */
-  CBlockHeader parentBlock;
+  CPureBlockHeader parentBlock;
 
 public:
 
@@ -161,26 +162,5 @@ public:
   static int getExpectedIndex (int nNonce, int nChainId, unsigned h);
 
 };
-
-/* Implement serialisation of auxpow in the block header.  Since this
-   depends on CAuxPow being available, it can not be defined there.  */
-template<typename Stream, typename Operation>
-  void
-  ReadWriteAuxPow (Stream& s, Operation ser_action,
-                   boost::shared_ptr<CAuxPow>& auxpow,
-                   int nType, const CBlockVersion& nBlockVersion)
-{
-  const int nVersion = nBlockVersion.GetBaseVersion ();
-
-  if (nBlockVersion.IsAuxpow ())
-    {
-      if (ser_action.ForRead ())
-        auxpow.reset (new CAuxPow());
-      assert (auxpow);
-      READWRITE (*auxpow);
-    }
-  else if (ser_action.ForRead ())
-    auxpow.reset ();
-}
 
 #endif // BITCOIN_AUXPOW_H

src/core/block.h
@@ -6,144 +6,13 @@
 #ifndef BITCOIN_CORE_BLOCK_H
 #define BITCOIN_CORE_BLOCK_H
 
+#include "auxpow.h"
 #include "core/transaction.h"
+#include "core/pureheader.h"
 #include "serialize.h"
 #include "uint256.h"
 
 #include <boost/shared_ptr.hpp>
-
-/**
- * Encapsulate a block version.  This takes care of building it up
- * from a base version, the modifier flags (like auxpow) and
- * also the auxpow chain ID.
- */
-class CBlockVersion
-{
-
-private:
-
-    /* Modifiers to the version.  */
-    static const int32_t VERSION_AUXPOW = (1 << 8);
-
-    /** Bits above are reserved for the auxpow chain ID.  */
-    static const int32_t VERSION_CHAIN_START = (1 << 16);
-
-    /** The version as integer.  Should not be accessed directly.  */
-    int32_t nVersion;
-
-public:
-
-    inline CBlockVersion()
-    {
-        SetNull();
-    }
-
-    ADD_SERIALIZE_METHODS;
-
-    template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
-        READWRITE(this->nVersion);
-    }
-
-    inline void SetNull()
-    {
-        nVersion = 0;
-    }
-
-    /**
-     * Extract the base version (without modifiers and chain ID).
-     * @return The base version./
-     */
-    inline int32_t GetBaseVersion() const
-    {
-        return nVersion % VERSION_AUXPOW;
-    }
-
-    /**
-     * Set the base version (apart from chain ID and auxpow flag) to
-     * the one given.  This should only be called when auxpow is not yet
-     * set, to initialise a block!
-     * @param nBaseVersion The base version.
-     */
-    void SetBaseVersion(int32_t nBaseVersion);
-
-    /**
-     * Extract the chain ID.
-     * @return The chain ID encoded in the version.
-     */
-    inline int32_t GetChainId() const
-    {
-        return nVersion / VERSION_CHAIN_START;
-    }
-
-    /**
-     * Set the chain ID.  This is used for the test suite.
-     * @param ch The chain ID to set.
-     */
-    inline void SetChainId(int32_t chainId)
-    {
-        nVersion %= VERSION_CHAIN_START;
-        nVersion |= chainId * VERSION_CHAIN_START;
-    }
-
-    /**
-     * Extract the full version.  Used for RPC results and debug prints.
-     * @return The full version.
-     */
-    inline int32_t GetFullVersion() const
-    {
-        return nVersion;
-    }
-
-    /**
-     * Set the genesis block version.  This must be a literal write
-     * through, to get the correct historic version.
-     * @param nGenesisVersion The version to set.
-     */
-    inline void SetGenesisVersion(int32_t nGenesisVersion)
-    {
-        nVersion = nGenesisVersion;
-    }
-
-    /**
-     * Check if the auxpow flag is set in the version.
-     * @return True iff this block version is marked as auxpow.
-     */
-    inline bool IsAuxpow() const
-    {
-        return nVersion & VERSION_AUXPOW;
-    }
-
-    /**
-     * Set the auxpow flag.  This is used for testing.
-     * @param auxpow Whether to mark auxpow as true.
-     */
-    inline void SetAuxpow (bool auxpow)
-    {
-        if (auxpow)
-            nVersion |= VERSION_AUXPOW;
-        else
-            nVersion &= ~VERSION_AUXPOW;
-    }
-
-    /**
-     * Check whether this is a "legacy" block without chain ID.
-     * @return True iff it is.
-     */
-    inline bool IsLegacy() const
-    {
-        return nVersion == 1;
-    }
-
-};
-
-/* There's a circular dependency between CBlockHeader ans CAuxPow.
-   These forward-declarations help us resolve it.  */
-class CAuxPow;
-template<typename Stream, typename Operation>
-  void ReadWriteAuxPow(Stream& s, Operation ser_action,
-                       boost::shared_ptr<CAuxPow>& auxpow,
-                       int nType, const CBlockVersion& nBlockVersion);
 
 /** Nodes collect new transactions into a block, hash them into a hash tree,
  * and scan through nonce values to make the block's hash satisfy proof-of-work
@@ -152,17 +21,9 @@ template<typename Stream, typename Operation>
  * in the block is a special one that creates a new coin owned by the creator
  * of the block.
  */
-class CBlockHeader
+class CBlockHeader : public CPureBlockHeader
 {
 public:
-    // header
-    static const int32_t CURRENT_VERSION=2;
-    CBlockVersion nVersion;
-    uint256 hashPrevBlock;
-    uint256 hashMerkleRoot;
-    uint32_t nTime;
-    uint32_t nBits;
-    uint32_t nNonce;
 
     // auxpow (if this is a merge-minded block)
     boost::shared_ptr<CAuxPow> auxpow;
@@ -176,38 +37,23 @@ public:
 
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
-        READWRITE(this->nVersion);
+        READWRITE(*(CPureBlockHeader*)this);
         nVersion = this->nVersion.GetBaseVersion();
-        READWRITE(hashPrevBlock);
-        READWRITE(hashMerkleRoot);
-        READWRITE(nTime);
-        READWRITE(nBits);
-        READWRITE(nNonce);
 
-        ReadWriteAuxPow(s, ser_action, auxpow, nType, this->nVersion);
+        if (this->nVersion.IsAuxpow())
+        {
+            if (ser_action.ForRead())
+                auxpow.reset (new CAuxPow());
+            assert(auxpow);
+            READWRITE(*auxpow);
+        } else if (ser_action.ForRead())
+            auxpow.reset();
     }
 
     void SetNull()
     {
-        nVersion.SetNull();
-        hashPrevBlock = 0;
-        hashMerkleRoot = 0;
-        nTime = 0;
-        nBits = 0;
-        nNonce = 0;
+        CPureBlockHeader::SetNull();
         auxpow.reset();
-    }
-
-    bool IsNull() const
-    {
-        return (nBits == 0);
-    }
-
-    uint256 GetHash() const;
-
-    int64_t GetBlockTime() const
-    {
-        return (int64_t)nTime;
     }
 
     /**

src/core/pureheader.cpp
@@ -1,0 +1,22 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "core/pureheader.h"
+
+#include "chainparams.h"
+#include "hash.h"
+#include "utilstrencodings.h"
+
+void CBlockVersion::SetBaseVersion(int32_t nBaseVersion)
+{
+    assert(nBaseVersion >= 1 && nBaseVersion < VERSION_AUXPOW);
+    assert(!IsAuxpow());
+    nVersion = nBaseVersion | (Params ().AuxpowChainId () * VERSION_CHAIN_START);
+}
+
+uint256 CPureBlockHeader::GetHash() const
+{
+    return Hash(BEGIN(nVersion), END(nNonce));
+}

src/core/pureheader.h
@@ -1,0 +1,197 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2013 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_CORE_PUREHEADER_H
+#define BITCOIN_CORE_PUREHEADER_H
+
+#include "serialize.h"
+#include "uint256.h"
+
+/**
+ * Encapsulate a block version.  This takes care of building it up
+ * from a base version, the modifier flags (like auxpow) and
+ * also the auxpow chain ID.
+ */
+class CBlockVersion
+{
+
+private:
+
+    /* Modifiers to the version.  */
+    static const int32_t VERSION_AUXPOW = (1 << 8);
+
+    /** Bits above are reserved for the auxpow chain ID.  */
+    static const int32_t VERSION_CHAIN_START = (1 << 16);
+
+    /** The version as integer.  Should not be accessed directly.  */
+    int32_t nVersion;
+
+public:
+
+    inline CBlockVersion()
+    {
+        SetNull();
+    }
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(this->nVersion);
+    }
+
+    inline void SetNull()
+    {
+        nVersion = 0;
+    }
+
+    /**
+     * Extract the base version (without modifiers and chain ID).
+     * @return The base version./
+     */
+    inline int32_t GetBaseVersion() const
+    {
+        return nVersion % VERSION_AUXPOW;
+    }
+
+    /**
+     * Set the base version (apart from chain ID and auxpow flag) to
+     * the one given.  This should only be called when auxpow is not yet
+     * set, to initialise a block!
+     * @param nBaseVersion The base version.
+     */
+    void SetBaseVersion(int32_t nBaseVersion);
+
+    /**
+     * Extract the chain ID.
+     * @return The chain ID encoded in the version.
+     */
+    inline int32_t GetChainId() const
+    {
+        return nVersion / VERSION_CHAIN_START;
+    }
+
+    /**
+     * Set the chain ID.  This is used for the test suite.
+     * @param ch The chain ID to set.
+     */
+    inline void SetChainId(int32_t chainId)
+    {
+        nVersion %= VERSION_CHAIN_START;
+        nVersion |= chainId * VERSION_CHAIN_START;
+    }
+
+    /**
+     * Extract the full version.  Used for RPC results and debug prints.
+     * @return The full version.
+     */
+    inline int32_t GetFullVersion() const
+    {
+        return nVersion;
+    }
+
+    /**
+     * Set the genesis block version.  This must be a literal write
+     * through, to get the correct historic version.
+     * @param nGenesisVersion The version to set.
+     */
+    inline void SetGenesisVersion(int32_t nGenesisVersion)
+    {
+        nVersion = nGenesisVersion;
+    }
+
+    /**
+     * Check if the auxpow flag is set in the version.
+     * @return True iff this block version is marked as auxpow.
+     */
+    inline bool IsAuxpow() const
+    {
+        return nVersion & VERSION_AUXPOW;
+    }
+
+    /**
+     * Set the auxpow flag.  This is used for testing.
+     * @param auxpow Whether to mark auxpow as true.
+     */
+    inline void SetAuxpow (bool auxpow)
+    {
+        if (auxpow)
+            nVersion |= VERSION_AUXPOW;
+        else
+            nVersion &= ~VERSION_AUXPOW;
+    }
+
+    /**
+     * Check whether this is a "legacy" block without chain ID.
+     * @return True iff it is.
+     */
+    inline bool IsLegacy() const
+    {
+        return nVersion == 1;
+    }
+
+};
+
+/**
+ * A block header without auxpow information.  This "intermediate step"
+ * in constructing the full header is useful, because it breaks the cyclic
+ * dependency between auxpow (referencing a parent block header) and
+ * the block header (referencing an auxpow).  The parent block header
+ * does not have auxpow itself, so it is a pure header.
+ */
+class CPureBlockHeader
+{
+public:
+    // header
+    static const int32_t CURRENT_VERSION=2;
+    CBlockVersion nVersion;
+    uint256 hashPrevBlock;
+    uint256 hashMerkleRoot;
+    uint32_t nTime;
+    uint32_t nBits;
+    uint32_t nNonce;
+
+    CPureBlockHeader()
+    {
+        SetNull();
+    }
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(this->nVersion);
+        nVersion = this->nVersion.GetBaseVersion();
+        READWRITE(hashPrevBlock);
+        READWRITE(hashMerkleRoot);
+        READWRITE(nTime);
+        READWRITE(nBits);
+        READWRITE(nNonce);
+    }
+
+    void SetNull()
+    {
+        nVersion.SetNull();
+        hashPrevBlock = 0;
+        hashMerkleRoot = 0;
+        nTime = 0;
+        nBits = 0;
+        nNonce = 0;
+    }
+
+    bool IsNull() const
+    {
+        return (nBits == 0);
+    }
+
+    uint256 GetHash() const;
+
+    int64_t GetBlockTime() const
+    {
+        return (int64_t)nTime;
+    }
+};
+
+#endif // BITCOIN_CORE_PUREHEADER_H

xaya/xaya

src/auxpow.h
@@ -10,11 +10,12 @@
 #include "serialize.h"
 #include "uint256.h"
 
-#include "primitives/block.h"
+#include "primitives/pureheader.h"
 #include "primitives/transaction.h"
 
 #include <vector>
 
+class CBlock;
 class CBlockIndex;
 
 /** Header for merge-mining data in the coinbase.  */
@@ -102,7 +103,7 @@ public:
   int nChainIndex;
 
   /** Parent block header (on which the real PoW is done).  */
-  CBlockHeader parentBlock;
+  CPureBlockHeader parentBlock;
 
 public:
 
@@ -161,26 +162,5 @@ public:
   static int getExpectedIndex (int nNonce, int nChainId, unsigned h);
 
 };
-
-/* Implement serialisation of auxpow in the block header.  Since this
-   depends on CAuxPow being available, it can not be defined there.  */
-template<typename Stream, typename Operation>
-  void
-  ReadWriteAuxPow (Stream& s, Operation ser_action,
-                   boost::shared_ptr<CAuxPow>& auxpow,
-                   int nType, const CBlockVersion& nBlockVersion)
-{
-  const int nVersion = nBlockVersion.GetBaseVersion ();
-
-  if (nBlockVersion.IsAuxpow ())
-    {
-      if (ser_action.ForRead ())
-        auxpow.reset (new CAuxPow());
-      assert (auxpow);
-      READWRITE (*auxpow);
-    }
-  else if (ser_action.ForRead ())
-    auxpow.reset ();
-}
 
 #endif // BITCOIN_AUXPOW_H

src/primitives/block.h
@@ -6,7 +6,9 @@
 #ifndef BITCOIN_PRIMITIVES_BLOCK_H
 #define BITCOIN_PRIMITIVES_BLOCK_H
 
+#include "auxpow.h"
 #include "primitives/transaction.h"
+#include "primitives/pureheader.h"
 #include "serialize.h"
 #include "uint256.h"
 
@@ -15,139 +17,6 @@
 /** The maximum allowed size for a serialized block, in bytes (network rule) */
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
 
-/**
- * Encapsulate a block version.  This takes care of building it up
- * from a base version, the modifier flags (like auxpow) and
- * also the auxpow chain ID.
- */
-class CBlockVersion
-{
-
-private:
-
-    /* Modifiers to the version.  */
-    static const int32_t VERSION_AUXPOW = (1 << 8);
-
-    /** Bits above are reserved for the auxpow chain ID.  */
-    static const int32_t VERSION_CHAIN_START = (1 << 16);
-
-    /** The version as integer.  Should not be accessed directly.  */
-    int32_t nVersion;
-
-public:
-
-    inline CBlockVersion()
-    {
-        SetNull();
-    }
-
-    ADD_SERIALIZE_METHODS;
-
-    template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
-        READWRITE(this->nVersion);
-    }
-
-    inline void SetNull()
-    {
-        nVersion = 0;
-    }
-
-    /**
-     * Extract the base version (without modifiers and chain ID).
-     * @return The base version./
-     */
-    inline int32_t GetBaseVersion() const
-    {
-        return nVersion % VERSION_AUXPOW;
-    }
-
-    /**
-     * Set the base version (apart from chain ID and auxpow flag) to
-     * the one given.  This should only be called when auxpow is not yet
-     * set, to initialise a block!
-     * @param nBaseVersion The base version.
-     */
-    void SetBaseVersion(int32_t nBaseVersion);
-
-    /**
-     * Extract the chain ID.
-     * @return The chain ID encoded in the version.
-     */
-    inline int32_t GetChainId() const
-    {
-        return nVersion / VERSION_CHAIN_START;
-    }
-
-    /**
-     * Set the chain ID.  This is used for the test suite.
-     * @param ch The chain ID to set.
-     */
-    inline void SetChainId(int32_t chainId)
-    {
-        nVersion %= VERSION_CHAIN_START;
-        nVersion |= chainId * VERSION_CHAIN_START;
-    }
-
-    /**
-     * Extract the full version.  Used for RPC results and debug prints.
-     * @return The full version.
-     */
-    inline int32_t GetFullVersion() const
-    {
-        return nVersion;
-    }
-
-    /**
-     * Set the genesis block version.  This must be a literal write
-     * through, to get the correct historic version.
-     * @param nGenesisVersion The version to set.
-     */
-    inline void SetGenesisVersion(int32_t nGenesisVersion)
-    {
-        nVersion = nGenesisVersion;
-    }
-
-    /**
-     * Check if the auxpow flag is set in the version.
-     * @return True iff this block version is marked as auxpow.
-     */
-    inline bool IsAuxpow() const
-    {
-        return nVersion & VERSION_AUXPOW;
-    }
-
-    /**
-     * Set the auxpow flag.  This is used for testing.
-     * @param auxpow Whether to mark auxpow as true.
-     */
-    inline void SetAuxpow (bool auxpow)
-    {
-        if (auxpow)
-            nVersion |= VERSION_AUXPOW;
-        else
-            nVersion &= ~VERSION_AUXPOW;
-    }
-
-    /**
-     * Check whether this is a "legacy" block without chain ID.
-     * @return True iff it is.
-     */
-    inline bool IsLegacy() const
-    {
-        return nVersion == 1;
-    }
-
-};
-
-/* There's a circular dependency between CBlockHeader ans CAuxPow.
-   These forward-declarations help us resolve it.  */
-class CAuxPow;
-template<typename Stream, typename Operation>
-  void ReadWriteAuxPow(Stream& s, Operation ser_action,
-                       boost::shared_ptr<CAuxPow>& auxpow,
-                       int nType, const CBlockVersion& nBlockVersion);
-
 /** Nodes collect new transactions into a block, hash them into a hash tree,
  * and scan through nonce values to make the block's hash satisfy proof-of-work
  * requirements.  When they solve the proof-of-work, they broadcast the block
@@ -155,17 +24,9 @@ template<typename Stream, typename Operation>
  * in the block is a special one that creates a new coin owned by the creator
  * of the block.
  */
-class CBlockHeader
+class CBlockHeader : public CPureBlockHeader
 {
 public:
-    // header
-    static const int32_t CURRENT_VERSION=3;
-    CBlockVersion nVersion;
-    uint256 hashPrevBlock;
-    uint256 hashMerkleRoot;
-    uint32_t nTime;
-    uint32_t nBits;
-    uint32_t nNonce;
 
     // auxpow (if this is a merge-minded block)
     boost::shared_ptr<CAuxPow> auxpow;
@@ -179,38 +40,23 @@ public:
 
     template <typename Stream, typename Operation>
     inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
-        READWRITE(this->nVersion);
+        READWRITE(*(CPureBlockHeader*)this);
         nVersion = this->nVersion.GetBaseVersion();
-        READWRITE(hashPrevBlock);
-        READWRITE(hashMerkleRoot);
-        READWRITE(nTime);
-        READWRITE(nBits);
-        READWRITE(nNonce);
 
-        ReadWriteAuxPow(s, ser_action, auxpow, nType, this->nVersion);
+        if (this->nVersion.IsAuxpow())
+        {
+            if (ser_action.ForRead())
+                auxpow.reset (new CAuxPow());
+            assert(auxpow);
+            READWRITE(*auxpow);
+        } else if (ser_action.ForRead())
+            auxpow.reset();
     }
 
     void SetNull()
     {
-        nVersion.SetBaseVersion(CBlockHeader::CURRENT_VERSION);
-        hashPrevBlock.SetNull();
-        hashMerkleRoot.SetNull();
-        nTime = 0;
-        nBits = 0;
-        nNonce = 0;
+        CPureBlockHeader::SetNull();
         auxpow.reset();
-    }
-
-    bool IsNull() const
-    {
-        return (nBits == 0);
-    }
-
-    uint256 GetHash() const;
-
-    int64_t GetBlockTime() const
-    {
-        return (int64_t)nTime;
     }
 
     /**

src/primitives/pureheader.cpp
@@ -1,0 +1,22 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "primitives/pureheader.h"
+
+#include "chainparams.h"
+#include "hash.h"
+#include "utilstrencodings.h"
+
+void CBlockVersion::SetBaseVersion(int32_t nBaseVersion)
+{
+    assert(nBaseVersion >= 1 && nBaseVersion < VERSION_AUXPOW);
+    assert(!IsAuxpow());
+    nVersion = nBaseVersion | (Params ().AuxpowChainId () * VERSION_CHAIN_START);
+}
+
+uint256 CPureBlockHeader::GetHash() const
+{
+    return Hash(BEGIN(nVersion), END(nNonce));
+}

src/primitives/pureheader.h
@@ -1,0 +1,197 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2013 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_PRIMITIVES_PUREHEADER_H
+#define BITCOIN_PRIMITIVES_PUREHEADER_H
+
+#include "serialize.h"
+#include "uint256.h"
+
+/**
+ * Encapsulate a block version.  This takes care of building it up
+ * from a base version, the modifier flags (like auxpow) and
+ * also the auxpow chain ID.
+ */
+class CBlockVersion
+{
+
+private:
+
+    /* Modifiers to the version.  */
+    static const int32_t VERSION_AUXPOW = (1 << 8);
+
+    /** Bits above are reserved for the auxpow chain ID.  */
+    static const int32_t VERSION_CHAIN_START = (1 << 16);
+
+    /** The version as integer.  Should not be accessed directly.  */
+    int32_t nVersion;
+
+public:
+
+    inline CBlockVersion()
+    {
+        SetNull();
+    }
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(this->nVersion);
+    }
+
+    inline void SetNull()
+    {
+        nVersion = 0;
+    }
+
+    /**
+     * Extract the base version (without modifiers and chain ID).
+     * @return The base version./
+     */
+    inline int32_t GetBaseVersion() const
+    {
+        return nVersion % VERSION_AUXPOW;
+    }
+
+    /**
+     * Set the base version (apart from chain ID and auxpow flag) to
+     * the one given.  This should only be called when auxpow is not yet
+     * set, to initialise a block!
+     * @param nBaseVersion The base version.
+     */
+    void SetBaseVersion(int32_t nBaseVersion);
+
+    /**
+     * Extract the chain ID.
+     * @return The chain ID encoded in the version.
+     */
+    inline int32_t GetChainId() const
+    {
+        return nVersion / VERSION_CHAIN_START;
+    }
+
+    /**
+     * Set the chain ID.  This is used for the test suite.
+     * @param ch The chain ID to set.
+     */
+    inline void SetChainId(int32_t chainId)
+    {
+        nVersion %= VERSION_CHAIN_START;
+        nVersion |= chainId * VERSION_CHAIN_START;
+    }
+
+    /**
+     * Extract the full version.  Used for RPC results and debug prints.
+     * @return The full version.
+     */
+    inline int32_t GetFullVersion() const
+    {
+        return nVersion;
+    }
+
+    /**
+     * Set the genesis block version.  This must be a literal write
+     * through, to get the correct historic version.
+     * @param nGenesisVersion The version to set.
+     */
+    inline void SetGenesisVersion(int32_t nGenesisVersion)
+    {
+        nVersion = nGenesisVersion;
+    }
+
+    /**
+     * Check if the auxpow flag is set in the version.
+     * @return True iff this block version is marked as auxpow.
+     */
+    inline bool IsAuxpow() const
+    {
+        return nVersion & VERSION_AUXPOW;
+    }
+
+    /**
+     * Set the auxpow flag.  This is used for testing.
+     * @param auxpow Whether to mark auxpow as true.
+     */
+    inline void SetAuxpow (bool auxpow)
+    {
+        if (auxpow)
+            nVersion |= VERSION_AUXPOW;
+        else
+            nVersion &= ~VERSION_AUXPOW;
+    }
+
+    /**
+     * Check whether this is a "legacy" block without chain ID.
+     * @return True iff it is.
+     */
+    inline bool IsLegacy() const
+    {
+        return nVersion == 1;
+    }
+
+};
+
+/**
+ * A block header without auxpow information.  This "intermediate step"
+ * in constructing the full header is useful, because it breaks the cyclic
+ * dependency between auxpow (referencing a parent block header) and
+ * the block header (referencing an auxpow).  The parent block header
+ * does not have auxpow itself, so it is a pure header.
+ */
+class CPureBlockHeader
+{
+public:
+    // header
+    static const int32_t CURRENT_VERSION=3;
+    CBlockVersion nVersion;
+    uint256 hashPrevBlock;
+    uint256 hashMerkleRoot;
+    uint32_t nTime;
+    uint32_t nBits;
+    uint32_t nNonce;
+
+    CPureBlockHeader()
+    {
+        SetNull();
+    }
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(this->nVersion);
+        nVersion = this->nVersion.GetBaseVersion();
+        READWRITE(hashPrevBlock);
+        READWRITE(hashMerkleRoot);
+        READWRITE(nTime);
+        READWRITE(nBits);
+        READWRITE(nNonce);
+    }
+
+    void SetNull()
+    {
+        nVersion.SetBaseVersion(CPureBlockHeader::CURRENT_VERSION);
+        hashPrevBlock = 0;
+        hashMerkleRoot = 0;
+        nTime = 0;
+        nBits = 0;
+        nNonce = 0;
+    }
+
+    bool IsNull() const
+    {
+        return (nBits == 0);
+    }
+
+    uint256 GetHash() const;
+
+    int64_t GetBlockTime() const
+    {
+        return (int64_t)nTime;
+    }
+};
+
+#endif // BITCOIN_PRIMITIVES_PUREHEADER_H