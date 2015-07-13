63e4f83d4c92083aef3aef8253f5dfa802249fb4
phelixnmc <githubnmc@blockchained.com>
2015-07-13 14:41:57 +0200

    guiconstants.h: Fix datadirpath / registry issue On Windows Namecore modifies the Bitcoin registry entry making Bitcoin disfunctional.

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -44,9 +44,9 @@ static const int MAX_URI_LENGTH = 255;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 35
 
-#define QAPP_ORG_NAME "Bitcoin"
-#define QAPP_ORG_DOMAIN "bitcoin.org"
-#define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
-#define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
+#define QAPP_ORG_NAME "Namecoin"
+#define QAPP_ORG_DOMAIN "namecoin.org"
+#define QAPP_APP_NAME_DEFAULT "Namecoin-Qt"
+#define QAPP_APP_NAME_TESTNET "Namecoin-Qt-testnet"
 
 #endif // BITCOIN_QT_GUICONSTANTS_H
