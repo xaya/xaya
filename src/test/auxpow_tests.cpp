ab23ed7b900c5b226b175d9bf981a5ed5fe0539a
Daniel Kraft <d@domob.eu>
2018-06-10 14:34:47 +0200

    Change include style from "" to <>.
    Update the auxpow_tests.cpp file, so that #include's are referenced with
    <> instead of "".  This makes it consistent with an upstream change that
    was done some time ago.

--- src/test/auxpow_tests.cpp
+++ src/test/auxpow_tests.cpp
@@ -2,18 +2,18 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "auxpow.h"
-#include "chainparams.h"
-#include "coins.h"
-#include "consensus/merkle.h"
-#include "validation.h"
-#include "pow.h"
-#include "primitives/block.h"
-#include "script/script.h"
-#include "utilstrencodings.h"
-#include "uint256.h"
+#include <auxpow.h>
+#include <chainparams.h>
+#include <coins.h>
+#include <consensus/merkle.h>
+#include <validation.h>
+#include <pow.h>
+#include <primitives/block.h>
+#include <script/script.h>
+#include <utilstrencodings.h>
+#include <uint256.h>
 
-#include "test/test_bitcoin.h"
+#include <test/test_bitcoin.h>
 
 #include <boost/test/unit_test.hpp>
 
