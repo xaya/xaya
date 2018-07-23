0d996799b4bcfcbece9218c6623893516fb91e25
Daniel Kraft <d@domob.eu>
2018-07-23 13:01:30 +0200

    New contrib script for bech32 address conversion.
    This adds a new script in contrib/namecoin (convertBechAddress.py and a
    helper convertBechAddress.sh wrapper).  The new script complements
    convertAddress.py for bech32, i.e. it allows to convert an arbitrary
    bech32 address (with some HRP) to another address with the same payload
    but a different HRP.
    
    This will be useful for updating test data from Bitcoin's to Namecoin's
    address format.

---
+++ contrib/namecoin/convertBechAddress.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+#   Convert Address - convert Bitcoin to Namecoin bech32 addresses
+#   Copyright (C) 2018  Daniel Kraft <d@domob.eu>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+import segwit_addr
+
+if len (sys.argv) not in [2, 3]:
+  print ("Usage: convertBechAddress.py ADDRESS [TO-HRP]")
+  sys.exit (-1);
+
+addr = sys.argv[1]
+if len (sys.argv) >= 3:
+  hrp = sys.argv[2]
+else:
+  hrp = "nc"
+
+oldHrp, data = segwit_addr.bech32_decode (addr)
+print ("Old HRP: %s" % oldHrp)
+newAddr = segwit_addr.bech32_encode (hrp, data)
+print (newAddr)
