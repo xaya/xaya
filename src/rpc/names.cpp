ac4bd9cf8997644df3518ddd1fa1a01a0fe56fc2
yanmaani <yanmaani@cock.li>
2020-07-24 12:00:00 +0000

    rpc: Amend copyright notice in names.cpp

--- src/rpc/names.cpp
+++ src/rpc/names.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2014-2020 Daniel Kraft
+// Copyright (c) 2020 yanmaani
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
