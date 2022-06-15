220e0ea022ef9d7de56bab2c43658f38d7198e0b
Daniel Kraft <d@domob.eu>
2022-06-15 15:53:04 +0200

    Update neoscrypt code.
    Update the neoscrypt code to the latest version from
    https://github.com/ghostlander/NeoScrypt.  There was seemingly some
    kind of bug in the previous version that made the hash wrong on newer
    compiler versions.

--- src/crypto/neoscrypt.h
+++ src/crypto/neoscrypt.h
@@ -2,10 +2,6 @@
 extern "C" {
 #endif
 
-#if defined(HAVE_CONFIG_H)
-#include "bitcoin-config.h"
-#endif
-
 void neoscrypt(const unsigned char *password, unsigned char *output,
   unsigned int profile);
 
@@ -17,11 +13,33 @@ void neoscrypt_copy(void *dstp, const void *srcp, unsigned int len);
 void neoscrypt_erase(void *dstp, unsigned int len);
 void neoscrypt_xor(void *dstp, const void *srcp, unsigned int len);
 
+#if defined(NEOSCRYPT_ASM) && defined(NEOSCRYPT_MINER_4WAY)
+void neoscrypt_4way(const unsigned char *password, unsigned char *output,
+  unsigned char *scratchpad);
+
+#ifdef NEOSCRYPT_SHA256
+void scrypt_4way(const unsigned char *password, unsigned char *output,
+  unsigned char *scratchpad);
+#endif
+
+void neoscrypt_blake2s_4way(const unsigned char *input,
+  const unsigned char *key, unsigned char *output);
+
+void neoscrypt_fastkdf_4way(const unsigned char *password,
+  const unsigned char *salt, unsigned char *output, unsigned char *scratchpad,
+  const unsigned int mode);
+#endif
+
 unsigned int cpu_vec_exts(void);
 
 #if (__cplusplus)
 }
 #else
+
+typedef unsigned long long ullong;
+typedef signed long long llong;
+typedef unsigned int uint;
+typedef unsigned char uchar;
 
 #ifndef MIN
 #define MIN(a, b) ((a) < (b) ? a : b)
@@ -34,21 +52,21 @@ unsigned int cpu_vec_exts(void);
 #define BLOCK_SIZE 64
 #define DIGEST_SIZE 32
 
-typedef unsigned char hash_digest[DIGEST_SIZE];
+typedef uchar hash_digest[DIGEST_SIZE];
 
 #define ROTL32(a,b) (((a) << (b)) | ((a) >> (32 - b)))
 #define ROTR32(a,b) (((a) >> (b)) | ((a) << (32 - b)))
 
 #define U8TO32_BE(p) \
-    (((unsigned int)((p)[0]) << 24) | ((unsigned int)((p)[1]) << 16) | \
-    ((unsigned int)((p)[2]) <<  8) | ((unsigned int)((p)[3])))
+    (((uint)((p)[0]) << 24) | ((uint)((p)[1]) << 16) | \
+    ((uint)((p)[2]) <<  8) | ((uint)((p)[3])))
 
 #define U32TO8_BE(p, v) \
-    (p)[0] = (unsigned char)((v) >> 24); (p)[1] = (unsigned char)((v) >> 16); \
-    (p)[2] = (unsigned char)((v) >>  8); (p)[3] = (unsigned char)((v)      );
+    (p)[0] = (uchar)((v) >> 24); (p)[1] = (uchar)((v) >> 16); \
+    (p)[2] = (uchar)((v) >>  8); (p)[3] = (uchar)((v)      );
 
 #define U64TO8_BE(p, v) \
-    U32TO8_BE((p),     (unsigned int)((v) >> 32)); \
-    U32TO8_BE((p) + 4, (unsigned int)((v)      ));
+    U32TO8_BE((p),     (uint)((v) >> 32)); \
+    U32TO8_BE((p) + 4, (uint)((v)      ));
 
 #endif
